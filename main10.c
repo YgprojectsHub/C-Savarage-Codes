8d1d0105c5a831e42b882b90ee43f68def8e0248
Yusuf Güngörür <89793711+YgprojectsHub@users.noreply.github.com>
2024-11-19 21:32:59 +0300

    Update main10.c

--- main10.c
+++ main10.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main10()
+int main()
 {
-    // Göstericiler - pointers
+    // GÃ¶stericiler - pointers
     
     int sayi= 5;
     int *ptr = &sayi;
